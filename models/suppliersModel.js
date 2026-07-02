1227bf31f4011088f307a0087d9f09e0ee0d3813
Victor Rosario <vros15@yahoo.com>
2026-07-02 11:36:03 -0400

    register route for retrieving all inventory items

--- models/suppliersModel.js
+++ models/suppliersModel.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-//create a schema for suppliers: Name : string required , unique, category: string , quanity: number default 0 , price: number, required , supplier: object id ref to suppliers, timestamp: date required
-const supplierObject = mongoose.Schema.Types.ObjectId;
-
-
 
 const suppliersSchema = new mongoose.Schema({
 
--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -3,11 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 //import item controller
-const { createItem} = require("../controllers/itemController");
+const { createItem,getAllItems} = require("../controllers/itemController");
 
 //create new item
 router.post("/", createItem);
 
+//get all items
+router.get("/", getAllItems);
 
 
 //export the router
