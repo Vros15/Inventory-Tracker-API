7625d94e91e0f5d9253f578ef33e104c3f589d9c
Victor Rosario <vros15@yahoo.com>
2026-07-02 11:34:49 -0400

    feat: add endpoint function  to retrieve all inventory items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -18,8 +18,20 @@ const createItem = async (req, res) => {
     }
 };
 
+//read all items in the inventory and populate the supplier information for each item.
+const getAllItems = async (req, res) => {
+    try{
+        const items = await Item.find().populate('supplier');
+        res.status(200).json(items);
+    }catch(error){
+        res.status(500).json({message: error.message})
+    };
+};
+
+
 //export the controller
 module.exports = {
     createItem,
+    getAllItems
 
 }
