138f497495e25c359c4e758afa1f287711105e6b
Victor Rosario <vros15@yahoo.com>
2026-07-02 11:23:31 -0400

    feat: implement POST /api/v1/items endpoint

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,3 +1,25 @@
 //import mongoose
 const mongoose = require("mongoose");
 
+//import item model
+const Item = require("../models/itemsModel");
+
+//import supplier model to reference in item model
+const Suppliers = require("../models/suppliersModel");
+
+//Create a new item in the inventory and associate it with a supplier.
+const createItem = async (req, res) => {
+    try{
+        const newItem = await Item.create(req.body);
+        res.status(201).json(newItem);
+
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+};
+
+//export the controller
+module.exports = {
+    createItem,
+
+}
